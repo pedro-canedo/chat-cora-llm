cc7c18800f4ae58b6a5f7768c12afdd8eb5bb0a0
Pedro-Augusto <prof.pedrobalhe@gmail.com>
2024-07-17 00:09:43 -0300

    updated

--- backend/api/fastapi/app.py
+++ backend/api/fastapi/app.py
@@ -18,66 +18,15 @@ MODEL_NAME = os.getenv("MODEL_NAME", "llama3")
 HEADERS = {"Content-Type": "application/json"}
 
 DEFAULT_OPTIONS = {
-    "num_keep": 5,
-    "seed": 42,
-    "num_predict": 100,
-    "top_k": 20,
-    "top_p": 0.9,
-    "tfs_z": 0.5,
-    "typical_p": 0.7,
-    "repeat_last_n": 33,
-    "temperature": 0.8,
-    "repeat_penalty": 1.2,
-    "presence_penalty": 1.5,
-    "frequency_penalty": 1.0,
-    "mirostat": 1,
-    "mirostat_tau": 0.8,
-    "mirostat_eta": 0.6,
-    "penalize_newline": True,
-    "numa": False,
-    "num_ctx": 1024,
-    "num_batch": 2,
-    "num_gpu": 1,
-    "main_gpu": 0,
-    "low_vram": False,
-    "f16_kv": True,
-    "vocab_only": False,
-    "use_mmap": True,
-    "use_mlock": False,
-    "num_thread": 8
+
+    "temperature": 0.5,
 }
 
 options = DEFAULT_OPTIONS.copy()
 
 
 class ModelOptions(BaseModel):
-    num_keep: Optional[int] = None
-    seed: Optional[int] = None
-    num_predict: Optional[int] = None
-    top_k: Optional[int] = None
-    top_p: Optional[float] = None
-    tfs_z: Optional[float] = None
-    typical_p: Optional[float] = None
-    repeat_last_n: Optional[int] = None
     temperature: Optional[float] = None
-    repeat_penalty: Optional[float] = None
-    presence_penalty: Optional[float] = None
-    frequency_penalty: Optional[float] = None
-    mirostat: Optional[int] = None
-    mirostat_tau: Optional[float] = None
-    mirostat_eta: Optional[float] = None
-    penalize_newline: Optional[bool] = None
-    numa: Optional[bool] = None
-    num_ctx: Optional[int] = None
-    num_batch: Optional[int] = None
-    num_gpu: Optional[int] = None
-    main_gpu: Optional[int] = None
-    low_vram: Optional[bool] = None
-    f16_kv: Optional[bool] = None
-    vocab_only: Optional[bool] = None
-    use_mmap: Optional[bool] = None
-    use_mlock: Optional[bool] = None
-    num_thread: Optional[int] = None
 
 
 @app.get("/")
